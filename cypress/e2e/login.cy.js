309576c56bebb1ec944023bce533fbaa05e2fbaa
Daniela Cardoso <daniela.rbc@gmail.com>
2023-11-04 15:23:33 -0300

    feat(login): password validations

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,6 +7,7 @@ const TEST_IDS = {
 
 const MESSAGES = {
   requiredUsernameError: "Epic sadface: Username is required",
+  requiredPasswordError: "Epic sadface: Password is required",
   unmatchedUsernameError:
     "Epic sadface: Username and password do not match any user in this service",
 }
@@ -32,6 +33,23 @@ describe("Login", () => {
   it("Login with wrong email - should show an error", () => {
     cy.get(TEST_IDS.usernameField).type("example")
     cy.get(TEST_IDS.passwordField).type(Cypress.env("PASSWORD"))
+    cy.get(TEST_IDS.loginButton).click()
+    cy.get(TEST_IDS.loginErrorMessage).should(
+      "have.text",
+      MESSAGES.unmatchedUsernameError,
+    )
+  })
+  it("Login with empty password - should show an error", () => {
+    cy.get(TEST_IDS.usernameField).type(Cypress.env("STANDARD_USER"))
+    cy.get(TEST_IDS.loginButton).click()
+    cy.get(TEST_IDS.loginErrorMessage).should(
+      "have.text",
+      MESSAGES.requiredPasswordError,
+    )
+  })
+  it("Login with wrong password - should show an error", () => {
+    cy.get(TEST_IDS.usernameField).type(Cypress.env("STANDARD_USER"))
+    cy.get(TEST_IDS.passwordField).type("example")
     cy.get(TEST_IDS.loginButton).click()
     cy.get(TEST_IDS.loginErrorMessage).should(
       "have.text",
