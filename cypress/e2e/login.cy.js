92a5bc9d315ba953208a7cc3fcf8b4b51daa2f89
Daniela Cardoso <daniela.rbc@gmail.com>
2023-11-04 15:19:48 -0300

    feat(login): organize selector and messages

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,28 +1,41 @@
+const TEST_IDS = {
+  usernameField: 'input[data-test="username"]',
+  passwordField: 'input[data-test="password"]',
+  loginButton: 'input[data-test="login-button"]',
+  loginErrorMessage: 'h3[data-test="error"]',
+}
+
+const MESSAGES = {
+  requiredUsernameError: "Epic sadface: Username is required",
+  unmatchedUsernameError:
+    "Epic sadface: Username and password do not match any user in this service",
+}
+
 describe("Login", () => {
   beforeEach("Visit", () => {
     cy.visit("/")
   })
   it("Success login", () => {
-    cy.get('input[data-test="username"]').type(Cypress.env("STANDARD_USER"))
-    cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
-    cy.get('input[data-test="login-button"]').click()
+    cy.get(TEST_IDS.usernameField).type(Cypress.env("STANDARD_USER"))
+    cy.get(TEST_IDS.passwordField).type(Cypress.env("PASSWORD"))
+    cy.get(TEST_IDS.loginButton).click()
     cy.url().should("include", "/inventory.html")
   })
   it("Login with empty email - should show an error", () => {
-    cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
-    cy.get('input[data-test="login-button"]').click()
-    cy.get('h3[data-test="error"]').should(
+    cy.get(TEST_IDS.passwordField).type(Cypress.env("PASSWORD"))
+    cy.get(TEST_IDS.loginButton).click()
+    cy.get(TEST_IDS.loginErrorMessage).should(
       "have.text",
-      "Epic sadface: Username is required",
+      MESSAGES.requiredUsernameError,
     )
   })
   it("Login with wrong email - should show an error", () => {
-    cy.get('input[data-test="username"]').type("example")
-    cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
-    cy.get('input[data-test="login-button"]').click()
-    cy.get('h3[data-test="error"]').should(
+    cy.get(TEST_IDS.usernameField).type("example")
+    cy.get(TEST_IDS.passwordField).type(Cypress.env("PASSWORD"))
+    cy.get(TEST_IDS.loginButton).click()
+    cy.get(TEST_IDS.loginErrorMessage).should(
       "have.text",
-      "Epic sadface: Username and password do not match any user in this service",
+      MESSAGES.unmatchedUsernameError,
     )
   })
 })
