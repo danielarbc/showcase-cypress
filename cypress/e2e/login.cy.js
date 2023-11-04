ee472b62a7e1002610915bad06d6e95ffa35bc05
Daniela Cardoso <daniela.rbc@gmail.com>
2023-11-04 15:12:26 -0300

    feat(login): email validations

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,28 @@
 describe("Login", () => {
-  it("Success login", () => {
+  beforeEach("Visit", () => {
     cy.visit("/")
+  })
+  it("Success login", () => {
     cy.get('input[data-test="username"]').type(Cypress.env("STANDARD_USER"))
     cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
     cy.get('input[data-test="login-button"]').click()
     cy.url().should("include", "/inventory.html")
+  })
+  it("Login with empty email - should show an error", () => {
+    cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
+    cy.get('input[data-test="login-button"]').click()
+    cy.get('h3[data-test="error"]').should(
+      "have.text",
+      "Epic sadface: Username is required",
+    )
+  })
+  it("Login with wrong email - should show an error", () => {
+    cy.get('input[data-test="username"]').type("example")
+    cy.get('input[data-test="password"]').type(Cypress.env("PASSWORD"))
+    cy.get('input[data-test="login-button"]').click()
+    cy.get('h3[data-test="error"]').should(
+      "have.text",
+      "Epic sadface: Username and password do not match any user in this service",
+    )
   })
 })
