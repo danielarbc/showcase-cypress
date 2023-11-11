a7c5272fa3a352ced2dce8f97763c22f5973fec0
Daniela Cardoso <daniela.rbc@gmail.com>
2023-11-11 16:14:02 -0300

    refactor(checkout): improve command to enable reuse

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,7 +1,12 @@
 import { faker } from "@faker-js/faker"
 
-const product = {
-  dataTestName: "backpack",
+const products = {
+  backpack: 0,
+  bikeLight: 1,
+  fleeceJacket: 2,
+  tShirt: 3,
+  tShirtRed: 4,
+  Onesie: 5,
 }
 const personalData = {
   firstName: faker.person.firstName(),
@@ -10,16 +15,52 @@ const personalData = {
 }
 
 describe("Checkout", () => {
-  beforeEach("Visit", () => {
+  beforeEach("Visit and Login", () => {
     cy.login()
   })
-  it("Success checkout", () => {
-    cy.addProductToCart(product.dataTestName)
-    cy.fillPersonalDataAtCheckout(
-      personalData.firstName,
-      personalData.lastName,
-      personalData.postalCode,
-    )
-    cy.validateCheckoutInfosAndFinishOrder()
+  context("Success Checkout", () => {
+    it("Buy one product", () => {
+      const productsList = [products.backpack]
+      cy.addProductToCart(productsList)
+      cy.goToCart()
+      cy.fillPersonalDataAtCheckout(
+        personalData.firstName,
+        personalData.lastName,
+        personalData.postalCode,
+      )
+      cy.validateCheckoutInfos(productsList)
+      cy.finishOrder()
+    })
+    it("Buy two products", () => {
+      const productsList = [products.backpack, products.fleeceJacket]
+      cy.addProductToCart(productsList)
+      cy.goToCart()
+      cy.fillPersonalDataAtCheckout(
+        personalData.firstName,
+        personalData.lastName,
+        personalData.postalCode,
+      )
+      cy.validateCheckoutInfos(productsList)
+      cy.finishOrder()
+    })
+    it("Buy all products", () => {
+      const productsList = [
+        products.backpack,
+        products.fleeceJacket,
+        products.Onesie,
+        products.bikeLight,
+        products.tShirt,
+        products.tShirtRed,
+      ]
+      cy.addProductToCart(productsList)
+      cy.goToCart()
+      cy.fillPersonalDataAtCheckout(
+        personalData.firstName,
+        personalData.lastName,
+        personalData.postalCode,
+      )
+      cy.validateCheckoutInfos(productsList)
+      cy.finishOrder()
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -59,12 +59,20 @@ Cypress.Commands.add("login", (user, password) => {
 
 // Checkout
 
-Cypress.Commands.add("addProductToCart", (dataTestProductName) => {
-  cy.get(`[data-test="add-to-cart-sauce-labs-${dataTestProductName}"]`).click()
+Cypress.Commands.add("addProductToCart", (productNumberList) => {
+  for (let index in productNumberList) {
+    cy.fixture("products.json").then((products) => {
+      const product = products[productNumberList[index]]
+      cy.log(product.productName)
+      cy.get(`[data-test="add-to-cart-${product.dataTestName}"]`).click()
+    })
+  }
+})
+
+Cypress.Commands.add("goToCart", () => {
   cy.get(".shopping_cart_link").click()
   cy.get('[data-test="checkout"]').click()
 })
-
 Cypress.Commands.add(
   "fillPersonalDataAtCheckout",
   (firstName, lastName, postalCode) => {
@@ -75,8 +83,46 @@ Cypress.Commands.add(
   },
 )
 
-Cypress.Commands.add("validateCheckoutInfosAndFinishOrder", () => {
-  cy.get(".inventory_item_name").should("have.text", "Sauce Labs Backpack")
+Cypress.Commands.add("validateCheckoutInfos", (productNumberList) => {
+  cy.fixture("products.json").then((products) => {
+    let productsList = []
+    let productNameList = []
+    let productDescriptionList = []
+    let productPriceList = []
+    for (let index in productNumberList) {
+      productsList.push(products[productNumberList[index]])
+      productNameList.push(productsList[index].productName)
+      productDescriptionList.push(productsList[index].productDescription)
+      productPriceList.push(productsList[index].productPrice)
+    }
+
+    cy.log("**Product Name**")
+    cy.get(".inventory_item_name")
+      .should("have.length", productNumberList.length)
+      .then(($els) => {
+        return Cypress._.map(Cypress.$.makeArray($els), "innerText")
+      })
+      .should("deep.equal", productNameList)
+
+    cy.log("**Product Desc**")
+    cy.get(".inventory_item_desc")
+      .should("have.length", productNumberList.length)
+      .then(($els) => {
+        // jQuery => Array => get "innerText" from each
+        return Cypress._.map(Cypress.$.makeArray($els), "innerText")
+      })
+      .should("deep.equal", productDescriptionList)
+
+    cy.log("**Product Price**")
+    cy.get(".inventory_item_price")
+      .should("have.length", productNumberList.length)
+      .then(($els) => {
+        // jQuery => Array => get "innerText" from each
+        return Cypress._.map(Cypress.$.makeArray($els), "innerText")
+      })
+      .should("deep.equal", productPriceList)
+  })
+
   cy.get(".summary_value_label").contains("SauceCard #31337")
   cy.get(".summary_value_label").contains("Free Pony Express Delivery!")
   cy.get(".summary_subtotal_label")
@@ -94,6 +140,8 @@ Cypress.Commands.add("validateCheckoutInfosAndFinishOrder", () => {
       "match",
       /Total: \${1,}([0-9]{1,}\.[0-9]{1,}|[0-9]{1,})\.([0-9]{2})/,
     )
+})
+Cypress.Commands.add("finishOrder", () => {
   cy.get('[data-test="finish"]').click()
   cy.get(".complete-header").contains("Thank you for your order!")
   cy.get(".complete-text").contains(
